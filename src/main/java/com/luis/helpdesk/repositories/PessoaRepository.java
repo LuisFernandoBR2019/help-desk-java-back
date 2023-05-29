dc5572013734352b018e0d83c6cb05353413aeba
Luis Fernando <nando_srs@hotmail.com>
2023-05-29 20:56:40 -0300

    Projeto Back-End

---
+++ src/main/java/com/luis/helpdesk/repositories/PessoaRepository.java
@@ -0,0 +1,13 @@
+package com.luis.helpdesk.repositories;
+
+import java.util.Optional;
+
+import com.luis.helpdesk.domain.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
+
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
+
+}
